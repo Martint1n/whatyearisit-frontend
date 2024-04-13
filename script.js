f713481c9e29f9c4883812739ee1832911c97c06
guilbert <guilbertmartin75@gmail.com>
2024-04-13 14:34:34 +0200

    dynamic data

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 fetch("https://year-vercel.vercel.app/year")
 .then(response => response.json())
 .then(data => {
-    document.querySelector("#year").textContent = data.Date
+    document.querySelector("#year").textContent = data.year
 })
 // }
 
