dde5a1be8bf0699473458f71207549191f45fda0
guilbert <guilbertmartin75@gmail.com>
2024-04-13 14:00:03 +0200

    link html and js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const fetchYear = function (){
-fetch("https://year-vercel.vercel.app/year")
-.then(response => response.json())
-.then(data => console.log(data))
-}
+// const fetchYear = function (){
+// fetch("https://year-vercel.vercel.app/year")
+// .then(response => response.json())
+// .then(data => console.log(data))
 
-fetchYear()
+console.log("test")
+// }
+
+// fetchYear()
